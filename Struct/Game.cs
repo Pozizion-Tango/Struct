e524245738ec1ce7b8f9dbd581a6a3335abe28ac
David F.N <99806140+Pozizion-Tango@users.noreply.github.com>
2023-02-08 21:12:25 +0100

    Better version

--- Struct/Game.cs
+++ Struct/Game.cs
@@ -9,23 +9,78 @@ namespace Struct
 {
     struct Blackjack
     {
-        public string[,] cards;
-
-    }
-    public class Pc
-    {
-        public Blackjack Shuffle()
+        public int Cardpoint(int y)//checks how many points the card choosen is worth.
         {
-            string[,] shuffle = new string[4, 13]
+            int[] points = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };//all but ace is there. since ace goes through a different process
+            return points[y];
+        }
+        public string[,] Shuffle()//will give the game a new deck of cards.
+        {
+           string[,] shuffle = new string[4, 13]
         { {"Clubs 2", "Clubs 3", "Clubs 4", "Clubs 5", "Clubs 6", "Clubs 7", "Clubs 8", "Clubs 9", "Clubs 10", "Clubs Jack", "Clubs Queen", "Clubs King", "Clubs Ace"},
          {"Hearts 2","Hearts 3","Hearts 4","Hearts 5","Hearts 6","Hearts 7","Hearts 8","Hearts 9","Hearts 10","Hearts Jack","Hearts Queen","Hearts King","Hearts Ace" },
          {"Diamonds 2","Diamonds 3","Diamonds 4","Diamonds 5","Diamonds 6","Diamonds 7","Diamonds 8","Diamonds 9","Diamonds 10","Diamonds Jack","Diamonds Queen","Diamonds King","Diamonds Ace" },
          {"Spades 2","Spades 3","Spades 4","Spades 5","Spades 6","Spades 7","Spades 8","Spades 9","Spades 10","Spades Jack","Spades Queen","Spades King","Spades Ace" } };
-            
+            return shuffle;//will give out a fresh deck of cards.
         }
     }
     public class Player
     {
-
+        Blackjack blackjack;
+        public List<string> ownedcard = new List<string>();//the player's cards
+        public int cardtotal;//how much the players have each when it comes to card value
+        Random rnd = new Random();
+        public void Reset()//reset the players entirely 
+        {
+            this.ownedcard.Clear();
+            this.cardtotal = 0;
+        }
+        public string[,] Pickcard(string[,] cards)//Caluclations on what card the player's get
+        {//cards is the ingame's avaliblecards
+            int x;
+            int y;
+            bool nonnull = true;//used to stop it when a card is picked.
+            while (nonnull)
+            {
+                x = rnd.Next(0, 4);
+                y = rnd.Next(0, 13);
+                if (cards[x,y] != "NULL")// cards[x,y] is what row of card's it's trying to pick, and checking if the card isn't NULL
+                {
+                    nonnull = false;//the card is not a NULL and can go through the system
+                    this.ownedcard.Add(cards[x, y]);//adds the card to the player's collection of cards
+                    cards[x, y] = "NULL";//replaces the card with a NULL  in the avaliblecards
+                    if (y != 12)//12 is really an ACE.
+                        this.cardtotal = this.cardtotal + blackjack.Cardpoint(y);
+                    else
+                    {
+                        if (this.cardtotal <= 10)//An ACE is different. It can either be 1 or 11. It will be 11 if the player got below 10 in cardtotal
+                            this.cardtotal = this.cardtotal + 11;
+                        else
+                            this.cardtotal = this.cardtotal + 1;//The ACE, if the player got over 10 in cardtotal. It will be turned into a 1 instead of 11
+                    }
+                }
+            }
+            return cards;//returns the cards to avaliblecards
+        }
+        public void Display()//used to show the info of a player
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            foreach (string card in this.ownedcard)//shows all the cards a player got
+            {
+                Console.Write(card + "  ");
+            }
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\r\n" + this.cardtotal);//shows how much the card's total is
+            Console.ResetColor();
+        }
+        public bool Checkbust()//used to check if a player is bust
+        {
+            bool bust;
+            if (this.cardtotal > 21)//if it's above 21 the player has lost, so it will return bust true.
+                bust = true;
+            else
+                bust = false;//it's below 21, so nothing will change.
+            return bust;
+        }
     }
 }
--- Struct/Program.cs
+++ Struct/Program.cs
@@ -5,19 +5,108 @@ namespace Struct
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Pc pc = new Pc();
-            Player player = new Player();
-            Console.ForegroundColor = ConsoleColor.Magenta; Console.Write("----------------"); Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.DarkBlue; Console.Write(" BLACK "); Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("----------------"); Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Magenta; Console.Write("----------------"); Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.DarkBlue; Console.Write(" JACK  "); Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("----------------"); Console.ResetColor();
-            Blackjack bj = new Blackjack();
-
-            bj.cards = pc.Shuffle();
+            bool stop = false;//bool here is used to stop the entire program when the user wishes
+            Player playeruser = new Player();//creates 2 players, one being you. Other being the "computer"
+            Player playerbot = new Player();
+            Blackjack blackjack = new Blackjack();//blackjack is made to keep all cards in if needed.
+            while (!stop)
+            {
+                Console.Clear();//clears last round's off the screen.
+                Console.ForegroundColor = ConsoleColor.Magenta; Console.Write("----------------"); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkBlue; Console.Write(" BLACK "); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("----------------"); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Magenta; Console.Write("----------------"); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkBlue; Console.Write(" JACK  "); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("----------------"); Console.ResetColor();
+                Console.WriteLine("\r\n\r\n1:Play\r\n\r\n2:Quit");//tells the user what they can do
+                string choice = Console.ReadLine();//choice is used to select between the If/elses
+                if(choice =="1")
+                {
+                    Console.Clear();
+                    bool reset = false;//this bool is used a lot in the program, to either stop the round, or end it if one loses
+                    while (!reset)
+                    {   
+                        string[,] avaliblecards = blackjack.Shuffle();//a new set of cards are created from blackjack. (avaliblecards) will slowly run out when new cards are picked.
+                        playeruser.Reset();//if they have any scores or cards they will be removed
+                        playerbot.Reset();
+                        avaliblecards = playeruser.Pickcard(avaliblecards);//the player get's 2 cards to start with, as per rules in blackjack.
+                        avaliblecards = playeruser.Pickcard(avaliblecards);
+                        while (!reset)
+                        {
+                            playeruser.Display();//will show off what cards the player have and their total count
+                            Console.WriteLine("1: Call For Another Card\r\n2:Play Cards\r\n0:Reset");
+                            choice = Console.ReadLine();//choice is again used to select what they player now wanna do.
+                            if (choice == "1")//for a new card
+                            {
+                                Console.Clear();
+                                avaliblecards = playeruser.Pickcard(avaliblecards);//gives player another card
+                                if (playeruser.Checkbust())//Checks if the player is over 21
+                                {
+                                    playeruser.Display();
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("You Lost! Press Any Key To Continue");//the player was over 21 and went bust. Games stops
+                                    Console.ResetColor();
+                                    Console.ReadKey();
+                                    reset = true;//Player got bust, so the game ends by turning reset to True
+                                }
+                            }
+                            else if (choice == "2")//the player have choosen to let to bot play, and will not be allowed to pick more cards
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Bot's Turn\r\n");
+                                while (!reset)
+                                {    
+                                    avaliblecards = playerbot.Pickcard(avaliblecards);//the bot picks up their first card
+                                    playerbot.Display();//shows what card the bot now owns.
+                                    if (playerbot.Checkbust())//checks if the bot is bust
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Green;
+                                        Console.WriteLine("Bot Lost, User win! Press Any Key To Continue");//if the bot loses. the user wins
+                                        Console.ResetColor();
+                                        Console.WriteLine("\r\nUser's cards");
+                                        playeruser.Display();
+                                        Console.ReadKey();
+                                        reset = true;//game stops now that the user won.
+                                    }
+                                    else if (playerbot.cardtotal > playeruser.cardtotal && playerbot.cardtotal < 21)//checks if the bot won over the user
+                                    {
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("You Lost! Press Any Key To Continue");//bot had less then 21 but more then the user.
+                                        Console.ResetColor();
+                                        Console.WriteLine("\r\nUser's cards");
+                                        playeruser.Display();
+                                        Console.ReadKey();
+                                        reset = true;//user lost. games resets
+                                    }
+                                }
+                            }
+                            else if (choice == "0")//user choose to reset the game. Don't really know why i added that as an option?
+                            {
+                                reset = true;
+                            }
+                            else//something was wrong in what the user picked. and is warned about so.
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Invalid option. Press Anything To Try Again");
+                                Console.ReadKey();
+                                Console.Clear();
+                            }
+                        }
+                    }
+                }
+                else if (choice == "2")//The user have quit the game, and it will now close.
+                {
+                    stop = true;
+                }
+                else//user typed in something that was not the option, and is warned.
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid option. Press Anything To Try Again");
+                    Console.ReadKey();
+                }
+            }
         }
     }
 }
